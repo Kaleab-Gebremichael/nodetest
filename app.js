939526f370bfe85070fa72b6c65c11c65f2fc54f
Kaleab <kaleabsolomon470@gmail.com>
2019-07-30 11:03:53 -0700

    Made a way to remove users from group

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.set("view engine", "ejs"); 
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 let arr = [];
+let eventCode = Math.floor(Math.random() * 100) + 1; // returns a random integer from 1 to 100;
 
 app.get('/', (req,res) => {
-    res.render('index', {data: arr});
+    res.render('index', {data: arr, eventCode: eventCode});
 });
 
 app.post('/', (req,res) => {
     arr.push(req.body.name);
-    setTimeout(() => {
-        res.redirect('/');  
-    }, 3000);
+    // setTimeout(() => {
+    //     res.redirect('/');
+    // }, 3000);
+
+    res.redirect('/');
+});
+
+app.post('/remove', (req,res) => {
+
+  var index = arr.indexOf(req.body.name);
+
+  arr.splice(index, 1);
+
+  res.redirect('/')
 });
 
 app.listen(3000, () => {
+
     console.log("Server Running On Port 3000");
 });
